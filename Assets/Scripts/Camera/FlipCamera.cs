6cab6fffcdaaf1005aa2269fcc58c745f450cd46
Ewan Dubois <ewan.dubois@live.fr>
2021-01-31 13:30:56 +0100

    Inversion caméra

--- Assets/Scripts/Camera/FlipCamera.cs
+++ Assets/Scripts/Camera/FlipCamera.cs
@@ -1,25 +1,41 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Camera))]
 public class FlipCamera : MonoBehaviour
 {
-
-    public Camera m_camera;
-    void OnPreCull()
+    public Camera camera;
+    private void Start()
     {
-        m_camera.ResetWorldToCameraMatrix();
-        m_camera.ResetProjectionMatrix();
-        m_camera.projectionMatrix = m_camera.projectionMatrix * Matrix4x4.Scale(new Vector3(-1, 1, 1));
+        camera = GetComponent<Camera>();
+
+        Matrix4x4 mat = camera.projectionMatrix;
+        mat *= Matrix4x4.Scale(new Vector3(-1, 1, 1));
+        camera.projectionMatrix = mat;
     }
 
+
+    /*void OnPreCull()
+    {
+        Matrix4x4 scale;
+        if (camera.aspect > 2)
+        {
+            scale = Matrix4x4.Scale(new Vector3(-1, 1, 1));
+        }
+        else
+        {
+            scale = Matrix4x4.Scale(new Vector3(1, -1, 1));
+        }
+        camera.ResetWorldToCameraMatrix();
+        camera.ResetProjectionMatrix();
+        camera.projectionMatrix = camera.projectionMatrix * scale;
+    }
     void OnPreRender()
     {
         GL.invertCulling = true;
     }
-
     void OnPostRender()
     {
         GL.invertCulling = false;
-    }
-
+    }*/
 }
