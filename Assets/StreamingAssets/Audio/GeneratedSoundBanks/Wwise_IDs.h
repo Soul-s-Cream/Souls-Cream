4f5f4e13971bbc95263e17fcbf51940cae167bee
Thomas Dupont <thomasdupont23@hotmail.fr>
2021-02-02 20:51:28 +0100

    Add OzMozEndSong

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -36,6 +36,7 @@ namespace AK
         static const AkUniqueID OZFOOTSTEPS = 2515146559U;
         static const AkUniqueID OZJUMP = 1595060770U;
         static const AkUniqueID OZLANDING = 1035518595U;
+        static const AkUniqueID OZMOZENDSONG = 1579895492U;
         static const AkUniqueID OZMOZMENUTHEME = 3278785808U;
         static const AkUniqueID OZMOZTHEMEAMBIENCES = 3147785334U;
         static const AkUniqueID OZTUTO = 567465712U;
