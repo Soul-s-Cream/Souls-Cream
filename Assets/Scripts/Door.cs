69b1781141a303e9961e941459b7642528f682ef
Ewan Dubois <ewan.dubois@live.fr>
2021-02-07 19:40:24 +0100

    Refactor : Door

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -3,17 +3,79 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+public enum Direction
+{
+    HAUT,
+    BAS,
+    GAUCHE,
+    DROITE
+}
+
+[RequireComponent(typeof(SpriteRenderer))]
 public class Door : Mecanism
 {
-    public float directionPorte = 5;
+    #region Public Field
+    [Tooltip("Direction de l'ouverture de la porte")]
+    public Direction openingDirection;
+    [Range(0f, 5f)]
+    [Tooltip("Durée de l'animation d'ouverture")]
+    public float openingTime = 1f;
+    #endregion
+
+    #region Private Fields
     private bool DoorOpen = false;
+    private SpriteRenderer spriteRender;
+    #endregion
+
+    private void Awake()
+    {
+        spriteRender = GetComponent<SpriteRenderer>();
+    }
 
     protected override void SwitchOn()
     {
         if (!DoorOpen)
         {
             DoorOpen = true;
-            transform.DOMoveY(transform.position.y + directionPorte, 1f);
+            switch(openingDirection)
+            {
+                case Direction.HAUT:
+                    transform.DOMoveY(transform.position.y + spriteRender.bounds.size.y, openingTime);
+                    break;
+                case Direction.BAS:
+                    transform.DOMoveY(transform.position.y - spriteRender.bounds.size.y, openingTime);
+                    break;
+                case Direction.GAUCHE:
+                    transform.DOMoveX(transform.position.x - spriteRender.bounds.size.x, openingTime);
+                    break;
+                case Direction.DROITE:
+                    transform.DOMoveX(transform.position.x + spriteRender.bounds.size.x, openingTime);
+                    break;
+            }
+        }
+    }
+
+    protected override void SwitchOff()
+    {
+        if (DoorOpen)
+        {
+            DoorOpen = false;
+
+            switch (openingDirection)
+            {
+                case Direction.HAUT:
+                    transform.DOMoveY(transform.position.y - spriteRender.bounds.size.y, openingTime);
+                    break;
+                case Direction.BAS:
+                    transform.DOMoveY(transform.position.y + spriteRender.bounds.size.y, openingTime);
+                    break;
+                case Direction.GAUCHE:
+                    transform.DOMoveX(transform.position.x + spriteRender.bounds.size.x, openingTime);
+                    break;
+                case Direction.DROITE:
+                    transform.DOMoveX(transform.position.x - spriteRender.bounds.size.x, openingTime);
+                    break;
+            }
         }
     }
 }
